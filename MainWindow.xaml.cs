e88a32caf36cb3ea4d591d5b4e30e078ebbfccc2
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-14 08:57:08 +0500

    Разработанный интерфейс MainWindow. Бартова

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace VinylRecordsApplication_Bartova
         {
             InitializeComponent();
         }
+
+        private void OpenRecordList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenRecordAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportRecord(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenManufacrurersList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenManufacrurersAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenSupplyList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenSupplyAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenStateList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenStateAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
