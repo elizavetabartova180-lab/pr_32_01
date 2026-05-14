061de8862e5a6a5840be9f802ff865b03de8b0f1
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-14 11:19:16 +0500

    Разработанная страница добавления и изменения состояний. Бартова

---
+++ Pages/State/Add.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace VinylRecordsApplication_Bartova.Pages.State
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        Classes.State changeState;
+
+        public Add(Classes.State state = null)
+        {
+            InitializeComponent();
+            if (state != null) { 
+                this.changeState = state;
+                this.tbName.Text = state.Name;
+                this. tbSubname.Text = state.Subname;
+                this.tbDescription.Text = state.Description;
+                addBth.Content = "Изменить";
+            }        
+        }
+
+        private void AddState(object sender, RoutedEventArgs e)
+        {
+            if(!String.IsNullOrEmpty(tbName.Text))
+                if (!String.IsNullOrEmpty((tbSubname.Text)))
+                {
+                    if (this.changeState == null)
+                    {
+                        Classes.State newState = new Classes.State();
+                        {
+                            Name = tbName.Text;
+                            Subname = tbSubname.Text;
+                            Description = tbDescription.Text;
+                        };
+                        newState.Save();
+                        MessageBox.Show($"Состояние{newState.Name} успешно добавлено.", "Уведомление");
+                        MainWindow.mainWindow.OpenPage(new Pages.State.Add(newState));
+                    }
+                    else
+                    {
+                        changeState.Name = tbName.Text;
+                        changeState.Subname = tbSubname.Text;
+                        changeState.Description = tbDescription.Text;
+                        changeState.Save(true);
+                        MessageBox.Show($"Состояние{changeState.Name} успешно изменено.", "Уведомление");
+                    }
+                    
+                }
+                else
+                        MessageBox.Show("Пожалуйста, укажите скоращённое наименование состояния", "Предупреждение");
+            else
+                MessageBox.Show("Пожалуйста, укажите скоращённое наименование состояния", "Предупреждение");
+        }
+    }
+}
--- Pages/State/Main.xaml.cs
+++ Pages/State/Main.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VinylRecordsApplication_Bartova.Pages.State
 {
@@ -20,9 +8,12 @@ namespace VinylRecordsApplication_Bartova.Pages.State
     /// </summary>
     public partial class Main : Page
     {
+        public IEnumerable<Classes.State> AllState = Classes.State.AllState();
         public Main()
         {
             InitializeComponent();
+            foreach (var State in AllState)
+                stateParent.Children.Add(new Pages.State.Elements.State(State, this));
         }
     }
 }
