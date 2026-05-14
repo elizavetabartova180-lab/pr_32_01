31f56097709431b64a5b7e6812756fec3c811172
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-14 09:39:36 +0500

    Разработанный класс State и Country. Бартова

---
+++ Classes/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VinylRecordsApplication_Bartova.Classes
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static IEnumerable<Country> AllCountries() {
+            List<Country> countries = new List<Country>();
+            DataTable requestCountrys = DBConnection.Connection("SELECT * FROM [dbo]. [Country]");
+            foreach (DataRow row in requestCountrys.Rows)
+                countries.Add(new Country()
+                {
+                    Id = Convert.ToInt32(row[0]),
+                    Name = row[1].ToString()
+                });
+                
+            return countries;
+        }
+    }
+}
