72f7a7658a8f4aed932a05fe0229201c6161b3d0
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-21 12:36:28 +0500

    Программирование страницы поставщика. Бартова

--- Pages/Manufacturer/Main.xaml.cs
+++ Pages/Manufacturer/Main.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VinylRecordsApplication_Bartova.Pages.Manufacturer
 {
@@ -20,9 +8,12 @@ namespace VinylRecordsApplication_Bartova.Pages.Manufacturer
     /// </summary>
     public partial class Main : Page
     {
+        public IEnumerable<Classes.Manufacturer> AllManufacturers = Classes.Manufacturer.AllManufacturers();
         public Main()
         {
             InitializeComponent();
+            foreach(Classes.Manufacturer manufacturer in AllManufacturers)
+                manufacterParent.Children.Add(new Manufacturer.Elements.Manufacturer(manufacturer, this));
         }
     }
 }
