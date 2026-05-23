0d9ea616473c371d54b7a6f6140d3a9b682ed894
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-23 18:36:57 +0500

    шаг 36 наполовину готов

--- Pages/Records/Main.xaml.cs
+++ Pages/Records/Main.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VinylRecordsApplication_Bartova.Classes;
 
 namespace VinylRecordsApplication_Bartova.Pages.Records
 {
@@ -20,9 +21,64 @@ namespace VinylRecordsApplication_Bartova.Pages.Records
     /// </summary>
     public partial class Main : Page
     {
+        public IEnumerable<Classes.State> AllState = Classes.State.AllState();
+        public IEnumerable<Classes.Record> AllRecords = Classes.Record.AllRecords();
+        public IEnumerable<Classes.Manufacturer> AllManufacturers = Classes.Manufacturer.AllManufacturers();
+        private bool CreateUI = false;
+        public List<Classes.Record> searchRecords;
         public Main()
         {
             InitializeComponent();
+            searchRecords = AllRecords.ToList();
+            CreateUI = true;
+            LoadAllRecord(AllRecords.ToList());
+            LoadAllManufacture();
+            LoadAllState();
         }
+        public void LoadRecord()
+        {
+            AllRecords = Classes.Record.AllRecords();
+            LoadAllRecord(AllRecords.ToList());
+        }
+        public void LoadAllState()
+        {
+            tbState.Items.Clear();
+            foreach(var state in AllState)
+                tbState.Items.Add(state.Name);
+            tbState.Items.Add("Выберите...");
+            tbState.SelectedIndex = tbState.Items.Count - 1;
+        }
+
+        public void LoadAllManufacture()
+        {
+            tbManufacturer.Items.Clear();
+            foreach(var manufscturer in AllManufacturers)
+                tbManufacturer.Items.Add(manufscturer.Name);
+            tbManufacturer.Items.Add("Выберите...");
+            tbManufacturer.SelectedIndex = tbManufacturer.Items.Count - 1;
+        }
+
+        public void LoadAllRecord(List<Classes.Record> AllRecords)
+        {
+            recordsParent.Children.Clear();
+            foreach(var record in AllRecords)
+                recordsParent.Children.Add(new Pages.Records.Elements.Record(record, this));
+        }
+        public void RecordFilter()
+        {
+            List<Classes.Record> FilterRecords = new List<Classes.Record>();
+            if (tbManufacturer.SelectedIndex != tbManufacturer.Items.Count -1)
+
+        }
+        private void FilterRecords(object sender, SelectionChangedEventArgs e)
+        {
+            if (CreateUI)
+                RecordFilter();
+        }
+        private void SearchRecords(object sender, KeyEventArgs e) =>
+            RecordFilter();
+
+
+
     }
 }
