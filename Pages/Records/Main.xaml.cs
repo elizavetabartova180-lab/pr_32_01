fd59843ac1153b557f64261791fa155f633d53b6
ADMIN <ADMIN@MSI>
2026-05-24 01:00:15 +0500

    Программирование страницы с пластинками. Бартова

--- Pages/Records/Main.xaml.cs
+++ Pages/Records/Main.xaml.cs
@@ -52,8 +52,8 @@ namespace VinylRecordsApplication_Bartova.Pages.Records
         public void LoadAllManufacture()
         {
             tbManufacturer.Items.Clear();
-            foreach(var manufscturer in AllManufacturers)
-                tbManufacturer.Items.Add(manufscturer.Name);
+            foreach(var manufacturer in AllManufacturers)
+                tbManufacturer.Items.Add(manufacturer.Name);
             tbManufacturer.Items.Add("Выберите...");
             tbManufacturer.SelectedIndex = tbManufacturer.Items.Count - 1;
         }
@@ -64,19 +64,55 @@ namespace VinylRecordsApplication_Bartova.Pages.Records
             foreach(var record in AllRecords)
                 recordsParent.Children.Add(new Pages.Records.Elements.Record(record, this));
         }
-        public void RecordFilter()
+        public void RecordsFilter()
         {
             List<Classes.Record> FilterRecords = new List<Classes.Record>();
-            if (tbManufacturer.SelectedIndex != tbManufacturer.Items.Count -1)
+            if (tbManufacturer.SelectedIndex != tbManufacturer.Items.Count - 1)
+            {
+                FilterRecords = AllRecords.Where(x => x.IdManufacturer ==
+                    AllManufacturers.Where(y => y.Name == tbManufacturer.SelectedItem.ToString()).First().Id).ToList();
+            }
+            else
+            {
+                FilterRecords = AllRecords.ToList();
+            }
 
+            if (tbState.SelectedIndex != tbState.Items.Count - 1)
+            {
+                FilterRecords = FilterRecords.FindAll(x => x.IdState ==
+                    AllState.Where(y => y.Name == tbState.SelectedItem.ToString()).First().Id);
+            }
+            if (tbName.Text != "")
+            {
+                if ("моно".Contains(tbName.Text.ToLower()))
+                {
+                    FilterRecords = FilterRecords.FindAll(x => x.Format == 0);
+                }
+                else if ("стерео".Contains(tbName.Text.ToLower()))
+                {
+                    FilterRecords = FilterRecords.FindAll(x => x.Format == 1);
+                }
+                else
+                {
+                    FilterRecords = FilterRecords.FindAll(
+                        x =>
+                            x.Name.ToLower().Contains(tbName.Text.ToLower()) ||
+                            x.Year.ToString().Contains(tbName.Text) ||
+                            x.Price.ToString().Contains(tbName.Text) ||
+                            x.Description.ToLower().Contains(tbName.Text.ToLower()));
+                }
+            }
+            searchRecords.Clear();
+            searchRecords = FilterRecords;
+            LoadAllRecord(FilterRecords);
         }
         private void FilterRecords(object sender, SelectionChangedEventArgs e)
         {
             if (CreateUI)
-                RecordFilter();
+                RecordsFilter();
         }
         private void SearchRecords(object sender, KeyEventArgs e) =>
-            RecordFilter();
+            RecordsFilter();
 
 
 
